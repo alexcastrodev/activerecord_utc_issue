be6c1e6308d9e34b6ae0eacbd1fdd4e6e15f6d74
Alex Oliveira <alexoliveira7x@gmail.com>
2024-08-22 20:27:57 +0000

    ta podre, mas funciona

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@
 
 require 'active_record'
 require 'minitest/autorun'
-require 'tzinfo'
 
 ActiveRecord::Base.establish_connection(
   adapter: "postgresql",
@@ -25,6 +24,22 @@ class CreateTimezonesTable < ActiveRecord::Migration[7.2]
 end
 
 class Timezone < ActiveRecord::Base
+  def item_timezone
+    ActiveSupport::TimeZone['Europe/Lisbon'].now.formatted_offset
+  end
+
+  def start_at=(input)
+    datetime = DateTime.parse(input)
+    new_datetime = DateTime.new(datetime.year, datetime.month, datetime.day, datetime.hour, datetime.minute, datetime.second, item_timezone)
+    super(new_datetime.in_time_zone('UTC'))
+  end
+
+
+  def end_at=(input)
+    datetime = DateTime.parse(input)
+    new_datetime = DateTime.new(datetime.year, datetime.month, datetime.day, datetime.hour, datetime.minute, datetime.second, item_timezone)
+    super(new_datetime.in_time_zone('UTC'))
+  end
 end
 
 
@@ -47,18 +62,17 @@ class TimezoneTest < Minitest::Test
 
 
     timezone_a = Timezone.create(start_at: first_start_at, end_at: first_end_at)
-    timezone_b = Timezone.create(start_at: second_start_at, end_at: second_end_at)
+    # timezone_b = Timezone.create(start_at: second_start_at, end_at: second_end_at)
 
-    puts "========= TIMEZONE A ============"
-    puts timezone_a.start_at
-    puts timezone_a.end_at
-    puts "========= TIMEZONE B ============"
-    puts timezone_b.start_at
-    puts timezone_b.end_at
-    puts "================================="
+    # puts "========= TIMEZONE A ============"
+    # puts timezone_a.start_at
+    # puts timezone_a.end_at
+    # puts "========= TIMEZONE B ============"
+    # puts timezone_b.start_at
+    # puts timezone_b.end_at
+    # puts "================================="
 
-    # -"2024-08-21 23:00:00 UTC"
-    # +"2024-08-22T02:00:00+0300"
-    assert_equal timezone_a.start_at.to_s, first_start_at
+    assert_equal timezone_a.start_at.to_s, "2024-08-22 01:00:00 UTC"
+    assert_equal timezone_a.end_at.to_s, "2024-08-23 15:00:00 UTC"
   end
 end
